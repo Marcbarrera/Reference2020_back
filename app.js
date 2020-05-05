4a8e92c28e19998b679560381d9037f679917e41
marcbarrera <barreramarinmarc@gmail.com>
2020-05-05 12:32:23 +0200

    reinicialització del projecte. Basic BAck-end creat

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require ('express');
+const app = express();
+const mongoose = require("mongoose");
+const morgan = require ('morgan');
+const dotenv = require ('dotenv');
+dotenv.config();
+
+
+mongoose
+.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+})
+.then(() => console.log('DB connected'))
+.catch(err => console.log('DB CONNECTION ERROR: ', err));
+
+
+
+const postRoutes = require ('./routes/post')
+
+app.use(morgan("dev"));
+
+app.use("/", postRoutes);
+
+const port = process.env.PORT || 8080;
+app.listen(port);
