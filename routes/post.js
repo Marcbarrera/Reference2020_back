b27e315f21e84b45701ec5b575bf074ff4ff05b4
marcbarrera <barreramarinmarc@gmail.com>
2020-05-06 21:17:02 +0200

    update i delete post fet

--- routes/post.js
+++ routes/post.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const {getPosts, createPost} = require("../controllers/post");
+const {getPosts, createPost, postsByUser, postById, isPoster,updatePost, deletePost } = require("../controllers/post");
 const { requireSignin} = require("../controllers/auth");//ojo amb això que fa que els posts només es puguin veure si s'ha fet loggin. video 47
 const { userById } = require("../controllers/user");
 const {createPostValidator} = require('../validator')
@@ -7,9 +7,18 @@ const {createPostValidator} = require('../validator')
 const router = express.Router();
 
 router.get("/", getPosts); //requireSignin-> veure els post nomes si s'ha fet signin. Actualitzacio. s'ha passat el middleware a crear post
-router.post("/post", requireSignin, createPostValidator, createPost);
+router.post("/post/new/:userId", requireSignin, createPost, createPostValidator);
+router.get("/posts/by/userId",requireSignin, postsByUser) //ojo requireSignin
+router.put('/post:postId', requireSignin, isPoster, updatePost)
+router.delete('/post:postId', requireSignin, isPoster, deletePost)
 
-// totes les rutes que tenen :userID. 
-router.param("userId", userById)
+
+
+
+// totes les rutes que contenen :userId, l'app executarà primer userByID(). 
+router.param("userId", userById);
+// totes les rutes que contenen :postId, l'app executarà primer postByID(). 
+
+router.param("postId", postById);
 
 module.exports = router;
